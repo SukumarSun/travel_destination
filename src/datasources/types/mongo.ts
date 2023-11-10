48117e82203af7fad46fe54105cc8990079bae47
sundar-godspeed <sukumar@godspeed.systems>
2023-11-10 12:34:13 +0530

    few changes

--- src/datasources/types/mongo.ts
+++ src/datasources/types/mongo.ts
@@ -4,7 +4,6 @@ import { MongoClient, MongoClientOptions } from 'mongodb';
 export default class DataSource extends GSDataSource {
     private databaseName: string;
     private collectionName: string;
-
     constructor(config: PlainObject) {
         super(config);
         this.databaseName = 'Cluster0'; 
@@ -23,14 +22,12 @@ export default class DataSource extends GSDataSource {
         const {data, meta: {fnNameInWorkflow}} = args;
         const method = fnNameInWorkflow.split('.')[2];
         try {
-            // Ensure the MongoDB client is initialized
             if (this.client) {
                 const database = this.client.db(this.databaseName);
                 const collection = database.collection(this.collectionName);
                 const response = await collection[method](data);
                 return Promise.resolve(new GSStatus(true, 200, undefined, data));
             }
-
         } catch (error) {
             throw error;
         }
